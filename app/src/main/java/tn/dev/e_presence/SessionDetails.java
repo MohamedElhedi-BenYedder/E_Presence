5c0aeeab6f788b7f028c1b32fc20f9ce62efe614
MohamedElhedi-BenYedder <mohamedelhedi.benyedder@supcom.tn>
2020-12-24 18:38:08 +0100

    IB

--- app/src/main/java/tn/dev/e_presence/SessionDetails.java
+++ app/src/main/java/tn/dev/e_presence/SessionDetails.java
@@ -2,13 +2,31 @@ package tn.dev.e_presence;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class SessionDetails extends AppCompatActivity {
-
+    ImageButton btn_rq;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_session_details);
+        btn_rq=findViewById(R.id.btn_qr);
+        btn_rq.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(v.getContext(),CreateSession.class);
+
+
+                //
+                startActivity(i);
+            }
+        });
+
+
     }
-}
+    }
