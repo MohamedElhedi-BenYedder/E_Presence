20f21c9fa35fdc2ed7d85b06290ab406a8805108
MohamedElhedi-BenYedder <mohamedelhedi.benyedder@supcom.tn>
2020-12-29 20:02:16 +0100

    SchoolDashboard

---
+++ app/src/main/java/tn/dev/e_presence/SchoolProfile.java
@@ -0,0 +1,14 @@
+package tn.dev.e_presence;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SchoolProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_school_profile);
+    }
+}
